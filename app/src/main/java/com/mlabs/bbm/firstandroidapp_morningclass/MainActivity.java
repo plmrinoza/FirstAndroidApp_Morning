c6be4133dbde587287c595147a89c3474a892a96
Daryl Jhay Crisostomo <Daryl Jhay Crisostomo>
2016-08-05 00:30:11 +0800

    updated

--- app/src/main/java/com/mlabs/bbm/firstandroidapp_morningclass/MainActivity.java
+++ app/src/main/java/com/mlabs/bbm/firstandroidapp_morningclass/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
 
         login=(Button)findViewById(R.id.loginButton);
         email=(EditText)findViewById(R.id.emailEditText);
