2b4a58d6a752c117543d24b2a6c1a38ce2791e5f
Homer C. Albino <jc_albino@yahoo.com>
2016-08-05 21:00:01 +0800

    validate email and password

--- app/src/main/java/com/mlabs/bbm/firstandroidapp_morningclass/MainActivity.java
+++ app/src/main/java/com/mlabs/bbm/firstandroidapp_morningclass/MainActivity.java
@@ -2,6 +2,16 @@ package com.mlabs.bbm.firstandroidapp_morningclass;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +19,50 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final EditText email = (EditText) findViewById(R.id.email);
+        final EditText password = (EditText) findViewById(R.id.password);
+        Button validate = (Button) findViewById(R.id.validate);
+
+        validate.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                if(!validateEmail(email.getText().toString())) {
+                    email.setError("Invalid Email");
+                    email.requestFocus();
+                } else if (!validatePassword(password.getText().toString())) {
+                    password.setError("Invalid Password");
+                    password.requestFocus();
+                } else {
+                    Toast.makeText(MainActivity.this, "Input Validation Success", Toast.LENGTH_LONG).show();
+                }
+
+            }
+        });
     }
+
+    //Return true if password is valid and false if password is invalid
+    protected boolean validatePassword(String password) {
+        if(password!=null && password.length()>8) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    //Return true if email is valid and false if email is invalid
+    protected boolean validateEmail(String email) {
+        String emailPattern = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+                + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+
+        Pattern pattern = Pattern.compile(emailPattern);
+        Matcher matcher = pattern.matcher(email);
+
+        return matcher.matches();
+    }
+
+
+
 }
+
