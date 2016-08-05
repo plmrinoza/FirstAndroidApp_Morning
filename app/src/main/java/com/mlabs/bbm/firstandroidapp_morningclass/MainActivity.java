bf3b85652f9e12b717499f90227e053777b681ac
Peter Lorenz Riñoza <ptrinoza@gmail.com>
2016-08-05 20:10:43 +0800

    created a splash screen, login screen validates email syntax and password length, if email and pass are valid directs the user to a blank screen.

--- app/src/main/java/com/mlabs/bbm/firstandroidapp_morningclass/MainActivity.java
+++ app/src/main/java/com/mlabs/bbm/firstandroidapp_morningclass/MainActivity.java
@@ -1,13 +1,48 @@
 package com.mlabs.bbm.firstandroidapp_morningclass;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button login;
+    EditText u, p;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        login = (Button) findViewById(R.id.button);
+        u = (EditText) findViewById(R.id.editText);
+        p = (EditText) findViewById(R.id.editText1);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isValidEmail(u.getText().toString(),p.getText().toString())){
+                    Intent intent = new Intent(MainActivity.this,blank.class );
+                    startActivity(intent);
+                }
+                else
+                    Toast.makeText(getApplicationContext(), "Invalid", Toast.LENGTH_LONG).show();
+            }
+        });
     }
-}
+        boolean isValidEmail(String x, String y) {
+            if (android.util.Patterns.EMAIL_ADDRESS.matcher(x).matches() && y.length()<8 && y.length()!=0){
+                return true;
+            }
+            else
+                return false;
+
+
+        }
+    }
---
+++ app/src/main/java/com/mlabs/bbm/firstandroidapp_morningclass/blank.java
@@ -0,0 +1,15 @@
+package com.mlabs.bbm.firstandroidapp_morningclass;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Pedro on 05/08/2016.
+ */
+
+public class blank extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.blank);
+    }
+}
