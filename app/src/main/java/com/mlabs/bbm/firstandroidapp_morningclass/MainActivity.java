d895669b5b521b0d1bd939cb3a8d13722ad6bd73
John Kenneth Fidelino <jkafidelino@gmail.com>
2016-08-11 15:56:38 +0800

    Sorry Sir nalate po nasira po kasi laptop ko ito na po Activity1&2

--- app/src/main/java/com/mlabs/bbm/firstandroidapp_morningclass/MainActivity.java
+++ app/src/main/java/com/mlabs/bbm/firstandroidapp_morningclass/MainActivity.java
@@ -1,13 +1,61 @@
 package com.mlabs.bbm.firstandroidapp_morningclass;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private static EditText Username;
+    private static EditText password;
+    private static TextView attempts;
+    private static Button Login_btn;
+    int attempt_counter = 5;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void LoginButton() {
+        Username = (EditText) findViewById(R.id.etUser);
+        password = (EditText) findViewById(R.id.etPass);
+        attempts = (TextView) findViewById(R.id.tvNumattempt);
+        Login_btn = (Button) findViewById(R.id.bLog);
+
+        attempts.setText(Integer.toString(attempt_counter));
+
+        Login_btn.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if (Username.getText().toString().equals("user") &&
+                                password.getText().toString().equals("pass")) {
+                            Toast.makeText(MainActivity.this, "User and Password is correct",
+                                    Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(MainActivity.this, UserAct.class);
+                            startActivity(intent);
+                        } else {
+                            Toast.makeText(MainActivity.this, "User and Password is not correct",
+                                    Toast.LENGTH_SHORT).show();
+                            attempt_counter--;
+                            attempts.setText(Integer.toString(attempt_counter));
+                            if (attempt_counter == 0) {
+                                Login_btn.setEnabled(false);
+                            }
+                        }
+
+                    }
+                }
+        );
+    }
 }
+
+
+
