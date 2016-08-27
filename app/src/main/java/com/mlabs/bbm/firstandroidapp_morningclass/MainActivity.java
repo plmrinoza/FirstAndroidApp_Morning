8735a448bb10d7b912a56e5bb6d4d0c64dac011c
Miguel Franco Gipulan <gipulan.mf@gmail.com>
2016-08-27 11:10:54 +0800

    hide and show of password

--- app/src/main/java/com/mlabs/bbm/firstandroidapp_morningclass/MainActivity.java
+++ app/src/main/java/com/mlabs/bbm/firstandroidapp_morningclass/MainActivity.java
@@ -3,9 +3,12 @@ package com.mlabs.bbm.firstandroidapp_morningclass;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.PasswordTransformationMethod;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Switch;
 import android.widget.Toast;
 
 import java.util.regex.Pattern;
@@ -17,10 +20,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         Button btn;
-        final EditText emailAdd,passWord;
+        final EditText emailAdd,passWord,Show;
 
+
+
+        Show = (EditText)findViewById(R.id.show);
         emailAdd=(EditText)findViewById(R.id.editText);
         passWord=(EditText)findViewById(R.id.editText3);
         btn = (Button)findViewById(R.id.button);
@@ -47,10 +52,47 @@ public class MainActivity extends AppCompatActivity {
                         Toast.makeText(getBaseContext(), "Invalid Email address!", Toast.LENGTH_SHORT).show();
 
 
-
                 }
             });
+
+
+            Show.setOnTouchListener(new View.OnTouchListener(){
+              @Override
+                public boolean onTouch(View view, MotionEvent motionEvent){
+//                  if(motionEvent.getAction() == MotionEvent.ACTION_DOWN)
+//                    {
+//                        passWord.setTransformationMethod(null);
+//                        Show.setText("Hide");
+//                        return true;
+//                    }
+//                  else
+//                  {
+//                   passWord.setTransformationMethod(new PasswordTransformationMethod());
+//                      return false;
+//                  }
+                switch (motionEvent.getAction())
+                {
+                    case MotionEvent.ACTION_DOWN:
+                        passWord.setTransformationMethod(null);
+                        return true;
+                    case MotionEvent.ACTION_UP:
+                        passWord.setTransformationMethod(new PasswordTransformationMethod());
+                         return false;
+                    default:
+                        return false;
+
+
+                }
+
+             }
+
+
+
+            });
+
+
         }
+
 
     }
 }
