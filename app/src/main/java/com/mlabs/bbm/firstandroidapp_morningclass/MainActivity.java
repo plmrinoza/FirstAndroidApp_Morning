e664191b8708112923951e3d5e940e767c15b160
Android Studio <androidstudio@Adamsons-iMac.local>
2016-08-27 11:38:29 +0800

    nagana na

--- app/src/main/java/com/mlabs/bbm/firstandroidapp_morningclass/MainActivity.java
+++ app/src/main/java/com/mlabs/bbm/firstandroidapp_morningclass/MainActivity.java
@@ -2,13 +2,16 @@ package com.mlabs.bbm.firstandroidapp_morningclass;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.PasswordTransformationMethod;
+import android.view.MotionEvent;
 import android.widget.EditText;
 import android.view.View;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import android.content.Intent;
 import android.view.View.OnClickListener;
-
+import android.view.View.OnTouchListener;
+import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity
@@ -16,43 +19,61 @@ public class MainActivity extends AppCompatActivity
 
     private EditText emailEditText;
     private EditText pasEditText;
+    private TextView show;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         emailEditText = (EditText) findViewById(R.id.eml);
         pasEditText = (EditText) findViewById(R.id.pass);
-        findViewById(R.id.button).setOnClickListener(new OnClickListener()
-        {
+        show = (TextView) findViewById(R.id.Show);
+
+        findViewById(R.id.button).setOnClickListener(new OnClickListener() {
 
             @Override
-            public void onClick(View arg0)
-            {
+            public void onClick(View arg0) {
 
                 final String email = emailEditText.getText().toString();
-                if (!isValidEmail(email))
-                {
+                if (!isValidEmail(email)) {
                     emailEditText.setError("Invalid Email");
                 }
 
                 final String pass = pasEditText.getText().toString();
-                if (!isValidPassword(pass))
-                {
+                if (!isValidPassword(pass)) {
                     pasEditText.setError("Invalid Password");
                 }
 
-                if (isValidEmail (email) && isValidPassword (pass))
-                {
-                    Intent intent = new Intent(MainActivity.this,Main2Activity.class);
+                if (isValidEmail(email) && isValidPassword(pass)) {
+                    Intent intent = new Intent(MainActivity.this, Main2Activity.class);
                     startActivity(intent);
                 }
 
             }
         });
+        show.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                switch (motionEvent.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        pasEditText.setTransformationMethod(null);
+                        return true;
+                    case MotionEvent.ACTION_UP:
+                        pasEditText.setTransformationMethod(new PasswordTransformationMethod());
+                        return false;
+
+                }
+                return false;
+
+
+            }
+        });
     }
+
+
+
+
 
 
     private boolean isValidEmail(String email)
@@ -73,9 +94,50 @@ public class MainActivity extends AppCompatActivity
         return false;
 
     }
-    @Override
-    protected  void onPause(){
-        super.onPause();
-        finish();
+
+  //  show=(TextView) findViewById(R.id.Show);
+    //
+    /*show=(TextView)findviewbyid(R.id.show);
+
+
+    R.id.show.setonclicktouch()
+    {
+        if(showpassword)
+        {
+            password.settransformationmethod(null);
+            show.settext()
+        }
     }
+    */
+
+    //showEditText = (TextView) findViewById(R.id.show);
+   /* showEditText.setOnTouchlistener(new View.OnTouchListener()
+    {
+      @Override
+              public boolean onTouch(View view, MotionEvent motionEvent)
+        {
+            int event = motionEvent getActon();
+            if(event == motionEvent.ACTION_DOWN)
+            {
+                Log.d("onTouchListener","ACTION_DOWN was pressed");
+                pasEditText.setTransformationMethod(null);
+                return true;
+            }
+            else
+            {
+                Log.d("onTouchListener","ACTION_DOWN was released");
+                pasEditText.setTransformationMethod(new PasswordTransformationMethod());
+                return false;
+            }
+
+        }
+    });*/
+
+
+            @Override
+            protected  void onPause(){
+            super.onPause();
+            finish();
+        }
 }
+
