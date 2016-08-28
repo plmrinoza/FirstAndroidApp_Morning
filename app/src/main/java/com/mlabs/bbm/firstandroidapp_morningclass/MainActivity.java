055aa2f19aba4a2278ae273e1c27f1d56a431e2c
girardEugenio9502 <girardeugenio0802@gmail.com>
2016-08-28 23:51:40 +0800

    Show Password Added

--- app/src/main/java/com/mlabs/bbm/firstandroidapp_morningclass/MainActivity.java
+++ app/src/main/java/com/mlabs/bbm/firstandroidapp_morningclass/MainActivity.java
@@ -1,59 +1,74 @@
 package com.mlabs.bbm.firstandroidapp_morningclass;
 
-        import android.content.Intent;
-        import android.support.v7.app.AppCompatActivity;
-        import android.os.Bundle;
-        import android.app.Activity;
-        import android.view.Menu;
-        import android.view.View;
-        import android.view.View.OnClickListener;
-        import android.widget.Button;
-        import android.widget.EditText;
-        import android.widget.TextView;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.method.PasswordTransformationMethod;
+import android.util.Log;
+import android.util.Patterns;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.view.View.OnClickListener;
+import android.app.Activity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
-    // User name
-    private EditText TFuname;
-    // Password
-    private EditText TFpassword;
-    // Sign In
-    private Button BLogin;
-    // Message
-    private TextView StatMsg;
+    EditText User, Pword, showPass;
+    Button AccessBtn;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected  void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Initialization
-        TFuname = (EditText) findViewById(R.id.TFuname);
-        TFpassword = (EditText) findViewById(R.id.TFpassword);
-        BLogin = (Button) findViewById(R.id.BLogin);
-        StatMsg = (TextView) findViewById(R.id.StatMsg);
+        AccessBtn = (Button) findViewById(R.id.BLogin);
+        User = (EditText) findViewById(R.id.Uname);
+        Pword = (EditText) findViewById(R.id.Pword);
+        showPass = (EditText) findViewById(R.id.show);
 
-        BLogin.setOnClickListener(new OnClickListener() {
+        AccessBtn.setOnClickListener(new View.OnClickListener() {
 
             @Override
-            public void onClick(View view) {
-                // Stores User name
-                String username = String.valueOf(TFuname.getText());
-                // Stores Password
-                String password = String.valueOf(TFpassword.getText());
-
-                // Validates the User name and Password for girardeugenio@gmail.com, 080295
-                if (username.equals("girardeugenio@gmail.com") && password.equals("080295")) {
-                    StatMsg.setText("Access Granted!");
-
-                    Intent intent = new Intent(MainActivity.this,Loggedin.class);
+            public void onClick(View v) {
+                if (validEmailadd(User.getText().toString(), Pword.getText().toString())) {
+                    Intent intent = new Intent(MainActivity.this, Loggedin.class);
                     startActivity(intent);
-
-                } else {
-                    StatMsg.setText("Access Denied!");
+                } else
+                    Toast.makeText(getApplicationContext(), "Invalid", Toast.LENGTH_LONG).show();
+            }
+        });
+        showPass.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        Log.d("event", "down");
+                        Pword.setTransformationMethod(null);
+                        return true;
+                    case MotionEvent.ACTION_UP:
+                        Log.d("event", "down");
+                        Pword.setTransformationMethod(new PasswordTransformationMethod());
+                        return true;
                 }
+                return false;
             }
         });
     }
 
+    boolean validEmailadd(String x, String y){
+        if (android.util.Patterns.EMAIL_ADDRESS.matcher(x).matches() && y.length() >= 8 && y.length() !=0){
+            return true;
+        }
+        else
+            return false;
+    }
+
+    @Override
+    protected  void onPause(){
+        super.onPause();
+        finish();
+    }
 }
