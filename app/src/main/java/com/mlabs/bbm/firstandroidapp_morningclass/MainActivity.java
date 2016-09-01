5a4eed7dc856e7dd16a895f9cd5541920d4a12ed
carlamonido <cmmonido@gmail.com>
2016-09-01 21:23:09 +0800

    added Show Password

--- app/src/main/java/com/mlabs/bbm/firstandroidapp_morningclass/MainActivity.java
+++ app/src/main/java/com/mlabs/bbm/firstandroidapp_morningclass/MainActivity.java
@@ -3,6 +3,9 @@ package com.mlabs.bbm.firstandroidapp_morningclass;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -23,9 +26,28 @@ public class MainActivity extends AppCompatActivity {
         etEmail = (EditText) findViewById(R.id.etEmail);
         etPW = (EditText) findViewById(R.id.etPW);
         findViewById(R.id.btnLogin).setOnClickListener(btnLoginClickListener);
+        findViewById(R.id.txtSP).setOnTouchListener(txtSPTouchListener);
 
         emailP = Pattern.compile("^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$");
     }
+
+    View.OnTouchListener txtSPTouchListener = new View.OnTouchListener(){
+        @Override
+        public boolean onTouch(View v, MotionEvent e) {
+            switch(e.getAction()){
+                case MotionEvent.ACTION_DOWN:
+                    etPW.setInputType(InputType.TYPE_CLASS_TEXT|InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+                    Log.d("ACTION","Pressed DOWN");
+                    return true;
+                case MotionEvent.ACTION_UP:
+                    etPW.setInputType(InputType.TYPE_CLASS_TEXT|InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                    Log.d("ACTION","Pressed UP");
+            }
+            Log.d("onTouch","Exit onTouch");
+            return false;
+        }
+    };
+
 
     View.OnClickListener btnLoginClickListener = new View.OnClickListener() {
         @Override
