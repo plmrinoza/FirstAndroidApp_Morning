56dc78b4e6be0d3916393a89d12a75eb948108cb
nabundo18 <nikko.adrianne.abundo@adamson.edu.ph>
2016-09-09 19:22:52 +0800

    Coordinate Locations

---
+++ app/src/main/java/com/mlabs/bbm/firstandroidapp_morningclass/TouchActivity.java
@@ -0,0 +1,55 @@
+package com.mlabs.bbm.firstandroidapp_morningclass;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class TouchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_touch);
+        final ImageView image = (ImageView) findViewById(R.id.imageView1);
+
+        image.setOnTouchListener(new View.OnTouchListener() {
+            float initX = 0, initY = 0, finalX = 0, finalY = 0;
+            String mss = "";
+
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                switch (motionEvent.getAction()){
+                    case MotionEvent.ACTION_DOWN:
+                        initX = motionEvent.getX();
+                        initY = motionEvent.getY();
+                        Toast.makeText(getApplicationContext(), ""+String.format("ACTION_DOWN>>>X:%.2f,Y:%.2f",initX, initY), Toast.LENGTH_SHORT).show();
+                        return true;
+                    case MotionEvent.ACTION_UP:
+                        finalX = motionEvent.getX();
+                        finalY = motionEvent.getY();
+                        if( initX<finalX){
+                            mss = String.format("SWIPED LEFT TO RIGHT");
+                        } if(initX > finalX){
+                        mss = String.format("SWIPED RIGHT TO LEFT");
+                    }
+                        Toast.makeText(getApplicationContext(), ""+mss, Toast.LENGTH_SHORT).show();
+                        if(initY < finalY){
+                            mss = String.format("SWIPED UP TO DOWN");
+                        } if(initY >finalY){
+                        mss = String.format("SWIPED DOWN TO UP");
+                    }
+                        Toast.makeText(getApplicationContext(), ""+mss, Toast.LENGTH_SHORT).show();
+                        return true;
+                }
+                return false;
+            }
+        });
+
+    }
+
+    Intent intent2 = getIntent();
+}
