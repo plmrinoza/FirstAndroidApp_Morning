7e8b6e5ae93366770400700559ce8f9db645ef79
Local <mhdyb.1221@gmail.com>
2016-09-20 16:55:44 +0800

    add DataBaseAdapter

---
+++ app/src/main/java/com/mlabs/bbm/firstandroidapp_morningclass/DataBaseAdapter.java
@@ -0,0 +1,105 @@
+package com.mlabs.bbm.firstandroidapp_morningclass;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+import android.content.ContentValues;
+import android.os.StrictMode;
+import java.util.HashMap;
+
+
+
+/**
+ * Created by LocalAd on 9/20/2016.
+ */
+public class DataBaseAdapter extends SQLiteOpenHelper {
+    private static final String TAG = DataBaseAdapter.class.getSimpleName();
+//DB Name
+    private static final String DATABASE_NAME = "users.db";
+//DB version
+    private static  final int DATABASE_VERSION = 1;
+//DB TableName
+    private static final String TABLE_USER = "user";
+//Defining Column names:
+    private static final String KEY_ID = "id";
+    private static final String KEY_EMAIL = "email";
+    private static final String KEY_PASSWORD = "password";
+    private static final String KEY_CREATE_AT = "create_at";
+
+    public DataBaseAdapter(Context _context){
+        super(_context, DATABASE_NAME,null,DATABASE_VERSION);
+    }
+
+//Define Database Table
+    @Override
+    public void onCreate(SQLiteDatabase sqlDB){
+        String CREATE_USER_TABLE = "CREATE TABLE" + TABLE_USER + "("
+                + KEY_ID + "INTEGER PRIMARY KEY,"
+                + KEY_EMAIL + "TEXT UNIQUE,"
+                + KEY_PASSWORD + "TEXT,"
+                + KEY_CREATE_AT + " TEXT " + ")";
+
+        sqlDB.execSQL(CREATE_USER_TABLE);
+        Log.d(TAG, "Database tables created");
+    }
+
+//Updating database
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion){
+        //Drop older table if existed
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_USER);
+
+        //Recreate Table
+        onCreate(db);
+    }
+
+    //Creating new user/s:
+    public void registerUser(String email, String password, String create_at){
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(KEY_EMAIL, email); //email
+        values.put(KEY_PASSWORD, password); //password
+        values.put(KEY_CREATE_AT, create_at); // created at
+
+        //Inserting Row
+        long id = db.insert(TABLE_USER, null, values);
+        db.close(); //close database connection
+
+        Log.d(TAG, "Successfully Added User " +id);
+    }
+
+    //Pulling records from Database
+    public boolean validateUser(String userName, String password){
+        HashMap<String,String> user = new HashMap<String, String>();
+        String selectQuery = "SELECT * FROM " + TABLE_USER + "WHERE" + KEY_EMAIL+ "=" + userName;
+
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery(selectQuery, null);
+        //Move to first row
+        cursor.moveToFirst();
+        if (cursor.getCount() > 0){
+            user.put("email", cursor.getString(1));
+            user.put("password", cursor.getString(2));
+            user.put("created_at", cursor.getString(3));
+        }
+        cursor.close();
+        db.close();
+        //return user record
+        Log.d(TAG, "Fetching user from Sqlite: " + user.toString());
+        //         Toast.makeText(DataBaseAdapter.this, "Password is incorrect", Toast.LENGTH_SHORT).show();
+        if(password.equals(user.get(password))){
+
+            Log.d(TAG, "Password was validated ");
+            return true;
+        }
+        else {
+
+            Log.d(TAG, "Password Incorrect ");
+            return false;
+        }
+    }
+
+}
