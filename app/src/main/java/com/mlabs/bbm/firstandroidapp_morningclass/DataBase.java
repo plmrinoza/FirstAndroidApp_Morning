f4addc170b3425896269172ca00dc4573b9ee1b6
OJT ISG <ojt.isg@int.wizardsgroup.com>
2016-09-22 18:22:11 +0800

    registration form

---
+++ app/src/main/java/com/mlabs/bbm/firstandroidapp_morningclass/DataBase.java
@@ -0,0 +1,139 @@
+package com.mlabs.bbm.firstandroidapp_morningclass;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.sql.SQLClientInfoException;
+
+/**
+ * Created by ojt.isg on 22/09/2016.
+ */
+public class DataBase extends SQLiteOpenHelper {
+
+    public static final String databaseName = "accountsAndroid.db";
+    public static final String TAG = DataBase.class.getSimpleName();
+    //table name
+    public static final String tableName = "accounts";
+    //public static final String tableName = "users";
+
+    //columns
+    public static final String accounts_ID = "ID";
+    public static final String accounts_EMAIL = "EMAIL";
+    public static final String accounts_PASSWORD = "PASSWORD";
+    // public static final String accounts_CREATED_AT = "CREATED_AT";
+
+
+    //public DatabaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+
+    public DataBase(Context context) {
+        super(context, databaseName, null, 1);
+        //For checking if database exists.
+        //SQLiteDatabase db = this.getWritableDatabase();
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        //   db.execSQL("Create table " + tableName + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, USERNAME TEXT, PASSWORD TEXT)");
+        String CREATE_ACCOUNT_TABLE = "Create Table " + tableName + "("
+                + accounts_ID + " INTEGER PRIMARY KEY,"
+                + accounts_EMAIL + " TEXT UNIQUE,"
+                + accounts_PASSWORD + " TEXT" + ")";
+        db.execSQL(CREATE_ACCOUNT_TABLE);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int olderVersion, int newVersion) {
+        // Drop older table if existed.
+        db.execSQL("DROP TABLE IF EXISTS " + tableName);
+        //Recreate Table.
+        onCreate(db);
+    }
+
+    public boolean insertAccount(String email, String password) {
+        String emailParameter = email;
+        String passwordParameter = password;
+
+        SQLiteDatabase db = this.getReadableDatabase();
+        ContentValues insertValues = new ContentValues();
+        insertValues.put("email", emailParameter);
+        insertValues.put("password", passwordParameter);
+        long result = db.insert(tableName, null, insertValues);
+
+        if (result == -1) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public boolean updateAccount(Integer id, String email, String password) {
+        String emailParameter = email;
+        String passwordParameter = password;
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues updatedValues = new ContentValues();
+        updatedValues.put("email", emailParameter);
+        updatedValues.put("password", passwordParameter);
+        db.update(tableName, updatedValues, "id = ? ", new String[]{Integer.toString(id)});
+        return true;
+    }
+    public String getSinlgeEntry(String userName) {
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.query(tableName, null, " EMAIL=?",
+                new String[] { userName }, null, null, null);
+        if (cursor.getCount() < 1) {
+            cursor.close();
+            return "NOT EXIST";
+        }
+        cursor.moveToFirst();
+        String password = cursor.getString(cursor.getColumnIndex("PASSWORD"));
+        cursor.close();
+        return password;
+    }
+
+   /* public boolean getUserDetails(String email, String password){
+        SQLiteDatabase db = this.getReadableDatabase();
+        HashMap<String, String> user = new HashMap<String, String>();
+        String query = "SELECT * FROM " + tableName + "WHERE " + accounts_EMAIL + "=" + email ;
+        Cursor cursor = db.rawQuery(query, null);
+        cursor.moveToNext();
+        if (cursor.getCount()>0){
+            user.put("email", cursor.getString(1));
+            user.put("password", cursor.getString(2));
+        }
+        cursor.close();
+        db.close();
+        Log.d(TAG, "Fetching user from database: " + user.toString());
+        if (password.equals(user.get(password))){
+            Log.d(TAG, "Correct Password");
+            return true;
+        }
+        else {
+            Log.d(TAG, "Incorrect Password");
+            return false;
+        }
+    } */
+
+
+    public Cursor getAllData() {
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        String[] columns = new String[] { accounts_ID,accounts_EMAIL,accounts_PASSWORD };
+        Cursor cursor = db.query(tableName, columns, null,
+                null, null, null, null);
+        return cursor;
+    }
+/*
+public Cursor getAllData() {
+    SQLiteDatabase db = this.getWritableDatabase();
+    Cursor  cursor = db.rawQuery("select * from "+ tableName,null);
+    return cursor;
+}
+*/
+
+
+
+}
--- app/src/main/java/com/mlabs/bbm/firstandroidapp_morningclass/RegistrationForm.java
+++ app/src/main/java/com/mlabs/bbm/firstandroidapp_morningclass/RegistrationForm.java
@@ -1,13 +1,131 @@
 package com.mlabs.bbm.firstandroidapp_morningclass;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class RegistrationForm extends AppCompatActivity {
+
+
+    DataBase accountsDb;
+
+
+    Button registerButton, viewAccountsButton, backButton;
+    EditText registerEmail, registerPassword, registerVerifyPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration_form);
+
+        accountsDb = new DataBase(this);
+
+
+        registerEmail = (EditText) findViewById(R.id.editText2);
+        registerPassword = (EditText) findViewById(R.id.editText4);
+        registerVerifyPassword = (EditText) findViewById(R.id.editText5);
+        registerButton = (Button) findViewById(R.id.button4);
+
+        backButton = (Button) findViewById(R.id.buttonBack);
+        addAccount();
+        viewAll();
+        back();
+    }
+
+    public void back(){
+        backButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent myIntent = new Intent(v.getContext(), MainActivity.class);
+                startActivityForResult(myIntent, 0);
+                onPause();
+                finish();
+            }
+        });
+    }
+
+
+    public void addAccount() {
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String email=registerEmail.getText().toString();
+                String password=registerPassword.getText().toString();
+                String verifyPassword = registerVerifyPassword.getText().toString();
+
+                if(email.equals("")||password.equals("")||verifyPassword.equals(""))
+                {
+                    Toast.makeText(getApplicationContext(), "requirement missing.", Toast.LENGTH_LONG).show();
+                    return;
+                }else if (!validateEmail(email)) {
+                    registerEmail.setError("input must be an email");
+                    registerEmail.requestFocus();
+                }
+                else if (!validatePassword(password)) {
+                    registerPassword.setError("atleast 10 characters");
+                    registerPassword.requestFocus();
+                }
+
+                else if(!password.equals(verifyPassword))
+                {
+                    Toast.makeText(getApplicationContext(), "Password does not match", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                else
+                {
+
+                    accountsDb.insertAccount(email, password);
+                    Toast.makeText(getApplicationContext(), "Account Successfully Created ", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+
+
+
+    public void viewAll() {
+
+
+
+
+    }
+
+
+
+    protected boolean validateEmail(String email) {
+        String emailPattern = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+                + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+
+        Pattern pattern = Pattern.compile(emailPattern);
+        Matcher matcher = pattern.matcher(email);
+
+        return matcher.matches();
+    }
+
+    protected boolean validatePassword(String password) {
+        if(password!=null && password.length()>9) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
     }
 }
