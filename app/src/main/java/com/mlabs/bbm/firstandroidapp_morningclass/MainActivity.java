fc88a6ab7d9ba5fc9cfc09967bde9fc43336c603
sverd <sverd>
2016-09-26 20:14:18 +0800

    Activity

--- app/src/main/java/com/mlabs/bbm/firstandroidapp_morningclass/MainActivity.java
+++ app/src/main/java/com/mlabs/bbm/firstandroidapp_morningclass/MainActivity.java
@@ -1,7 +1,6 @@
 package com.mlabs.bbm.firstandroidapp_morningclass;
 
 import android.content.Intent;
-<<<<<<< HEAD
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.method.PasswordTransformationMethod;
@@ -15,29 +14,17 @@ import android.widget.TextView;
 
 import com.mlabs.bbm.firstandroidapp_morningclass.MainAct.DatabaseHelper;
 
-=======
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
->>>>>>> daf1a508abdefe32f94f5c4ee200722e92e1d82b
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+public class MainActivity extends AppCompatActivity {
 
-<<<<<<< HEAD
     DatabaseHelper accountsDb;
 
-=======
-public class MainActivity extends AppCompatActivity {
->>>>>>> daf1a508abdefe32f94f5c4ee200722e92e1d82b
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-<<<<<<< HEAD
         accountsDb = new DatabaseHelper(this);
 
         final EditText loginEmail = (EditText) findViewById(R.id.editText_user;
@@ -112,54 +99,3 @@ public class MainActivity extends AppCompatActivity {
                 }
                             }
     }
-=======
-
-        Button login1 = (Button) findViewById(R.id.login);
-        login1.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                final EditText Email1 = (EditText) findViewById(R.id.Email);
-                final EditText Pw1 = (EditText) findViewById(R.id.Pw);
-
-                String email = Email1.getText().toString();
-                String password = Pw1.getText().toString();
-
-                if (!validateEmail(email)) {
-                    Email1.setError("Not a valid email address!");
-                } else if (!validatePassword(password)) {
-                    Pw1.setError("Not a valid password!");
-                } else {
-                    Email1.setError(null);
-                    Pw1.setError(null);
-                    doLogin();
-                }
-            }
-        });
-    }
-
-    private static final String EMAIL_PATTERN = "^[a-zA-Z0-9#_~!$&'()*+,;=:.\"(),:;<>@\\[\\]\\\\]+@[a-zA-Z0-9-]+(\\.[a-zA-Z0-9-]+)*$";
-    private Pattern pattern = Pattern.compile(EMAIL_PATTERN);
-    private Matcher matcher;
-
-    public boolean validateEmail(String email) {
-        matcher = pattern.matcher(email);
-        return matcher.matches();
-    }
-
-    public boolean validatePassword(String Pw) {
-        return Pw.length() >=8;
-    }
-
-    public void doLogin() {
-        Toast.makeText(getApplicationContext(), "Successfully Logged-in", Toast.LENGTH_SHORT).show();
-        Intent i = new Intent(MainActivity.this, NextScreen.class);
-
-        startActivity(i);
-
-
-    }
-
-}
-
-
-
->>>>>>> daf1a508abdefe32f94f5c4ee200722e92e1d82b
