bb9abb7d605719b17e6848e10cf2f535aa5ac92d
Mary Lauren Barredo <Mary Lauren Barredo>
2016-09-26 19:30:04 +0800

    modifies database adapter and registration, duplicate and username validation

--- app/src/main/java/com/mlabs/bbm/firstandroidapp_morningclass/DatabaseAdapter.java
+++ app/src/main/java/com/mlabs/bbm/firstandroidapp_morningclass/DatabaseAdapter.java
@@ -7,120 +7,111 @@ import android.util.Log;
 import android.content.ContentValues;
 import java.util.HashMap;
 import android.database.Cursor;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import java.util.HashMap;
+
+public class DatabaseAdapter extends SQLiteOpenHelper{
+    private static final String TAG = DatabaseAdapter.class.getSimpleName();
+    private static final String DATABASE_NAME = "users.db";
+    private static final int DATABASE_VERSION = 1;
+    private static final String TABLE_USER = "user";
+    private static final String KEY_ID = "id";
+    private static final String KEY_EMAIL = "email";
+    private static final String KEY_FIRST = "first";
+    private static final String KEY_SUR = "surname";
+    private static final String KEY_USER = "username";
+    private static final String KEY_PASSWORD = "password";
+    private static final String KEY_CREATED_AT = "created_at";
 
 
-
-public class DatabaseAdapter extends SQLiteOpenHelper {
-    private static final String TAG= DatabaseAdapter.class.getSimpleName();
-    private static final String DATABASE_NAME= "user.db";
-    private static final int DATABASE_VERSION= 1;
-    private static final String TABLE_USER= "user";
-    private static final String KEY_ID= "id";
-    private static final String KEY_EMAIL= "email";
-    private static final String KEY_PASSWORD= "password";
-    private static final String KEY_CREATED_AT= "created at";
-    private static final String KEY_FIRST_NAME= "firstname";
-    private static final String KEY_LAST_NAME= "lastname";
-    private static final String KEY_USER_NAME= "username";
-
-
-    static final String DATABASE_CREATE = "create table" + "LOGIN" + "(" + "ID" + "integer primary key autoincrement," + "Email text, password text);" + "FirstName text, LastName text);";
-
-    public SQLiteDatabase db;
-    private final Context context;
-    private DatabaseHelper dbHelper;
-
-    public DatabaseAdapter(Context _context){
-        super(_context,DATABASE_NAME,null, DATABASE_VERSION);
-        context= _context;
-        dbHelper = new DatabaseHelper (_context, DATABASE_NAME,null, DATABASE_VERSION);
-
+    public DatabaseAdapter(Context context) {
+        super(context, DATABASE_NAME,  null,    DATABASE_VERSION);
     }
 
-    public DatabaseAdapter open() throws SQLException {
-        db = dbHelper.getWritableDatabase();
-        return this;
-    }
-
-    public void close(){
-        db.close();
-    }
-    public SQLiteDatabase getDb(){
-        return db;
-    }
-
-   @Override
-    public void onCreate(SQLiteDatabase db){
-        String CREATE_USER_TABLE= "CREATE TABLE" + TABLE_USER + "("
-                + KEY_ID + "INTEGER PRIMARY KEY"
-                + KEY_EMAIL + "TEXT UNIQUE"
-                + KEY_PASSWORD + "TEXT,"
-                + KEY_FIRST_NAME + "TEXT,"
-                + KEY_LAST_NAME + "TEXT,"
-                + KEY_USER_NAME + "TEXT,"
-                + KEY_CREATED_AT + "TEXT" + ")";
-       db.execSQL(CREATE_USER_TABLE);
-
-    }
     @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion){
-        db.execSQL("DROP TABLE IF EXISTS" + TABLE_USER);
+    public void onCreate(SQLiteDatabase db) {
+        String CREATE_USER_TABLE = "CREATE TABLE " + TABLE_USER + "(" +
+                KEY_ID + " INTEGER PRIMARY KEY, " +
+                KEY_FIRST + " TEXT, " +
+                KEY_SUR + " TEXT, " +
+                KEY_USER+ " TEXT UNIQUE, " +
+                KEY_EMAIL + " TEXT UNIQUE, " +
+                KEY_PASSWORD + " TEXT, " +
+                KEY_CREATED_AT + " TEXT)";
+        db.execSQL(CREATE_USER_TABLE);
+        Log.d(TAG,"Database created");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_USER);
         onCreate(db);
     }
 
-    public  void registerUser(String email,String firstname, String lastname, String username, String password, String created_at){
-        SQLiteDatabase db= this.getWritableDatabase();
-        ContentValues values= new ContentValues();
+    public void registeruser(String first, String sur, String username, String email, String password, String created_at){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(KEY_FIRST,first);
+        values.put(KEY_SUR,sur);
+        values.put(KEY_USER,username);
         values.put(KEY_EMAIL,email);
-        values.put(KEY_FIRST_NAME,firstname);
-        values.put(KEY_LAST_NAME,lastname);
-        values.put(KEY_USER_NAME,username);
         values.put(KEY_PASSWORD,password);
         values.put(KEY_CREATED_AT,created_at);
 
-        Long id = db.insert(TABLE_USER,null, values);
+        long id  = db.insert(TABLE_USER,null, values);
         db.close();
-
-        Log.d(TAG, "Successfully added user: " + id);
-
+        Log.d(TAG, "User added successfully!");
     }
 
-    public boolean validateUser(String userName, String password){
-        HashMap<String,String> user= new HashMap<String,String>();
-        String selectQuery = "SELECT email, password FROM" + TABLE_USER + " WHERE " + KEY_EMAIL + "=\"" + userName+ "\" AND + KEY_PASSWORD + |=\""+password+ "\"";
-
+    public boolean checkExist(String username, String email){
+        String qry = "SELECT * FROM " + TABLE_USER + " WHERE " + KEY_EMAIL +" = '"+email+"' or "+ KEY_USER + " = '" + username +"'";
         SQLiteDatabase db = this.getReadableDatabase();
-        Cursor cursor  = db.rawQuery(selectQuery,null);
-        boolean result = false;
+        Cursor cursor = db.rawQuery(qry,null);
         cursor.moveToFirst();
-        if (cursor.getCount()>0){
-            user.put(KEY_EMAIL, cursor.getString(1));
-            user.put(KEY_PASSWORD, cursor.getString(2));
-            user.put(KEY_FIRST_NAME, cursor.getString(3));
-            user.put(KEY_LAST_NAME, cursor.getString(4));
-            user.put(KEY_USER_NAME, cursor.getString(5));
-            user.put("created_at", cursor.getString(6));
-
-
-           result= true;
-        }
-        else{
-            result =  false;
-        }
-        cursor.close();
-        db.close();
-
-
-        Log.d(TAG, "Fetching user for SQLite" + user.toString());
-        if(password.equals(user.get(password))){
-            Log.d(TAG, "Password was validated!");
+        if (cursor.getCount() != 0){
             return true;
         }
         else{
-            Log.d(TAG, "Password was incorrect");
             return false;
         }
     }
 
+    public boolean validateUser(String email, String password){
+        HashMap<String, String> user = new HashMap<String, String>();
+        String selectQuery = "SELECT * FROM "+TABLE_USER+" WHERE " +KEY_EMAIL+ "='" + email+"' AND " + KEY_PASSWORD + "="+password;
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery(selectQuery,null);
 
+        cursor.moveToFirst();
+        if (cursor.getCount()>0){
+            user.put("first",cursor.getString(1));
+            user.put("surname",cursor.getString(2));
+            user.put("username",cursor.getString(3));
+            user.put("email",cursor.getString(4));
+            user.put("password",cursor.getString(5));
+            user.put("created_at",cursor.getString(6));
+            Log.d(TAG,"Fetching user from Sqlite: "+user.toString());
+            cursor.close();
+            db.close();
+            return true;
+        }
+        else{
+            cursor.close();
+            db.close();
+            return false;
+        }
+    }
+
+    public  void deleteUser() {
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.delete(TABLE_USER, null, null);
+        db.close();
+        Log.d(TAG, "Deleted all user from sqlite");
+    }
 }
--- app/src/main/java/com/mlabs/bbm/firstandroidapp_morningclass/Registerform.java
+++ app/src/main/java/com/mlabs/bbm/firstandroidapp_morningclass/Registerform.java
@@ -19,147 +19,116 @@ import java.util.Date;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
-public class Registerform extends AppCompatActivity {
-    DatabaseAdapter db;
-
-    Button btnSend;
-    EditText regEmail, regPass, regConPass, firstName,lastName, userName;
-
-
+public class Registerform extends Activity{
+    EditText first, sur, user, email,pwd1,pwd2;
+    Button reg;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
-        Context context = this;
-        final DatabaseAdapter DataBaseAdapter;
-        DataBaseAdapter = new DatabaseAdapter(this);
+        first = (EditText)findViewById(R.id.etFirst);
+        sur = (EditText)findViewById(R.id.etLast);
+        user = (EditText)findViewById(R.id.etUsername);
+        email = (EditText)findViewById(R.id.regEmail);
+        pwd1 = (EditText)findViewById(R.id.regPass);
+        pwd2 = (EditText)findViewById(R.id.regConPass);
+        reg = (Button)findViewById(R.id.btnSend);
 
-
-
-        DataBaseAdapter.open();
-
-        regEmail = (EditText) findViewById(R.id.regEmail);
-        regPass = (EditText) findViewById(R.id.regPass);
-        regConPass = (EditText) findViewById(R.id.regConPass);
-        btnSend = (Button) findViewById(R.id.btnSend);
-        firstName= (EditText) findViewById(R.id.etFirst);
-        lastName= (EditText)findViewById(R.id.etLast);
-        userName= (EditText)findViewById(R.id.etUsername) ;
-        addAcct();
-
-    }
-
-
-    public void addAcct() {
-        btnSend.setOnClickListener(new View.OnClickListener() {
-                                       @Override
-                                       public void onClick(View v) {
-                                           String email = regEmail.getText().toString().trim();
-                                           String password = regPass.getText().toString();
-                                           String confirmPassword = regConPass.getText().toString();
-                                           final String fName= firstName.getText().toString();
-                                           final String lName= lastName.getText().toString();
-                                           final String uName= userName.getText().toString();
-
-                                           final String email1 = regEmail.getText().toString();
-                                           final String pass1 = regPass.getText().toString();
-                                           final String conPass1 = regConPass.getText().toString();
-
-
-
-                                           if (email.equals("") || (password.equals("") || (confirmPassword.equals("")))) {
-                                               Toast.makeText(getApplicationContext(), "Please complete all Fields", Toast.LENGTH_LONG).show();
-                                               return;
-
-                                           }
-                                           if (!isValidEmail(email1)) {
-                                               regEmail.setError("Invalid Email");
-
-
-                                           } else if (!isValidPassword(pass1)) {
-                                               regPass.setError("Invalid Password");
-
-
-                                           }
-                                           if(!isValidName(fName)){
-                                               firstName.setError("Invalid first name");
-
-                                           }
-                                           else if(!isValidName(lName)){
-                                               lastName.setError("Invalid first name");
-                                           }
-
-                                           if(!isValidUserName(uName)){
-                                               userName.setError("Invalid first name");
-                                           }
-
-                                           if (!pass1.equals(conPass1)) {
-                                               Toast.makeText(getApplicationContext(), "Password does not match!", Toast.LENGTH_LONG).show();
-                                               return;
-                                           } else {
-
-                                               DatabaseAdapter.registerUser(email1, pass1, GetCurrentDateAndTime());
-                                               Toast.makeText(getApplicationContext(), "Account Successfully Created ", Toast.LENGTH_LONG).show();
-                                               Intent i = new Intent(Registerform.this, MainActivity.class);
-                                               startActivity(i);
-                                               finish();
-
-                                           }
-
-                                       }
-
-
-                                        private boolean isValidName(String name){
-                                            String namePattern = "^[a-zA-Z ]+$";
-                                            Pattern pattern1 = Pattern.compile(namePattern);
-                                            Matcher matcher1= pattern1.matcher(name);
-                                            return matcher1.matches();
-
-
-                                        }
-                                        private boolean isValidUserName(String UserName){
-                                            String namePattern = "^[a-z0-9_-]{3,15}$";
-                                            Pattern pattern1 = Pattern.compile(namePattern);
-                                            Matcher matcher1= pattern1.matcher(UserName);
-                                            return matcher1.matches();
-                                        }
-
-
-                                       private boolean isValidEmail(String email) {
-                                           String emailPattern = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
-                                                   + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
-                                           Pattern pattern = Pattern.compile(emailPattern);
-                                           Matcher matcher = pattern.matcher(email);
-                                           return matcher.matches();
-                                       }
-
-
-                                       private boolean isValidPassword(String password) {
-                                           if (password != null && password.length() > 8) {
-                                               return true;
-                                           }
-                                           return false;
-
-                                       }
-
-                                       public String GetCurrentDateAndTime() {
-                                           DateFormat dateformat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
-                                           Date date = new Date();
-                                           System.out.println(dateformat.format(date));
-                                           return dateformat.format(date).toString();
-
-                                       }
-                                   }
-        );
+        reg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (check()) {
+                    DatabaseAdapter sqlDB = new DatabaseAdapter(Registerform.this);
+                    if (sqlDB.checkExist(user.getText().toString().trim(), email.getText().toString().trim())){
+                        Toast.makeText(getApplicationContext(),"Registration failed! Email/username already in use.", Toast.LENGTH_SHORT).show();
+                    }
+                    else {
+                        sqlDB.registeruser("","","",email.getText().toString().trim(),pwd1.getText().toString().trim(),getDate());
+                        Toast.makeText(getApplicationContext(),"Registration success!", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(Registerform.this, MainActivity.class);
+                        startActivity(intent);
+                    }
+                }
+                else {
+                    Toast.makeText(getApplicationContext(),"Registration failed", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
     @Override
-    public void onStart() {
-        super.onStart();
+    public void onPause(){
+        super.onPause();
+        super.finish();
+    }
+
+    public Boolean check(){
+        String _first = first.getText().toString();
+        String _surname = sur.getText().toString();
+        String _user = user.getText().toString();
+        String _email = email.getText().toString();
+        String _pwd1 = pwd1.getText().toString();
+        String _pwd2 = pwd2.getText().toString();
+
+        Pattern email_pattern = Pattern.compile("^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+                + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$");
+        Pattern name = Pattern.compile("^[A-Z][a-z]+( [A-Z][a-z]+)?$");
+        Matcher email = email_pattern.matcher(_email);
+        Matcher user = email_pattern.matcher(_user);
+        Matcher fname = name.matcher(_first);
+        Matcher sname = name.matcher(_surname);
 
 
+        if (_first.isEmpty() || !fname.matches()) {
+            Toast.makeText(getApplicationContext(),"Invalid First name", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        if (_surname.isEmpty() || !sname.matches()) {
+            Toast.makeText(getApplicationContext(),"Invalid Surname", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        if (_user.isEmpty() || user.matches()) {
+            Toast.makeText(getApplicationContext(),"Invalid username", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        if (_email.isEmpty() || !email.matches()) {
+            Toast.makeText(getApplicationContext(),"Invalid email", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        if (_pwd1.isEmpty() || _pwd2.isEmpty()) {
+            Toast.makeText(getApplicationContext(),"Password is/are empty", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        if (_pwd1.length() < 8 || _pwd2.length() < 8){
+            Toast.makeText(getApplicationContext(),"Password too short. Should be 8 characters or longer", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        if (!_pwd1.equals(_pwd2)) {
+            Toast.makeText(getApplicationContext(),"Passwords do not match", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        return true;
+    }
+
+    public String getDate(){
+        DateFormat df = new SimpleDateFormat("EEEE, d MMM yyyy, HH:mm");
+        String date = df.format(Calendar.getInstance().getTime());
+        return date;
     }
 }
