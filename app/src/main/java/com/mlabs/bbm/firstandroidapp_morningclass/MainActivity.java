6a2030624e27e131cea086250d2e1f8ab7cf3f87
SimonCuazon <sdgsai123@gmail.com>
2016-09-30 22:27:07 +0800

    Login and Registration Validation

--- app/src/main/java/com/mlabs/bbm/firstandroidapp_morningclass/MainActivity.java
+++ app/src/main/java/com/mlabs/bbm/firstandroidapp_morningclass/MainActivity.java
@@ -1,67 +1,97 @@
 package com.mlabs.bbm.firstandroidapp_morningclass;
 
+import android.content.ContentValues;
 import android.content.Intent;
-import android.os.Bundle;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-import java.util.regex.Matcher;
+
+import java.util.StringTokenizer;
 import java.util.regex.Pattern;
 
-
 public class MainActivity extends AppCompatActivity {
+    int x = 0;
+    public SQLiteDatabase myDB;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        try {
+            myDB = this.openOrCreateDatabase("Register", MODE_PRIVATE, null);
+            myDB.execSQL("CREATE TABLE IF NOT EXISTS Register (Username VARCHAR, Email VARCHAR, Password VARCHAR,Firstname VARCHAR, Lastname VARCHAR);");
+            //Toast.makeText(getBaseContext(), "Database Created", Toast.LENGTH_SHORT).show();
 
-        Button login1 = (Button) findViewById(R.id.login);
-        login1.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                final EditText Email1 = (EditText) findViewById(R.id.Email);
-                final EditText Pw1 = (EditText) findViewById(R.id.Pw);
+        }finally {
 
-                String email = Email1.getText().toString();
-                String password = Pw1.getText().toString();
+        }
 
-                if (!validateEmail(email)) {
-                    Email1.setError("Not a valid email address!");
-                } else if (!validatePassword(password)) {
-                    Pw1.setError("Not a valid password!");
-                } else {
-                    Email1.setError(null);
-                    Pw1.setError(null);
-                    doLogin();
-                }
-            }
-        });
     }
+    public void sai(View v) {
+        final EditText eText=(EditText)findViewById(R.id.editText);
+        final EditText pText=(EditText)findViewById(R.id.editText2);
+        String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+        String passPattern = "([a-zA-Z0-9]+_?)+";
+        switch (v.getId()) {
+            case R.id.button:
 
-    private static final String EMAIL_PATTERN = "^[a-zA-Z0-9#_~!$&'()*+,;=:.\"(),:;<>@\\[\\]\\\\]+@[a-zA-Z0-9-]+(\\.[a-zA-Z0-9-]+)*$";
-    private Pattern pattern = Pattern.compile(EMAIL_PATTERN);
-    private Matcher matcher;
+                String email= eText.getText().toString();
+                String passw= pText.getText().toString();
 
-    public boolean validateEmail(String email) {
-        matcher = pattern.matcher(email);
-        return matcher.matches();
+                    if (pText.getText().length()>=1 && eText.getText().length()>=1) {
+                            if (pText.getText().toString().trim().matches(passPattern)) {
+                                if (pText.getText().length() >= 8) {
+                                    Cursor cursor1 =myDB.query("Register",null,"Username=?",new String[]{email},null,null,null);
+                                    Cursor cursor2 =myDB.query("Register",null,"Email=?",new String[]{email},null,null,null);
+                                    if(cursor1.getCount()>0){
+                                        cursor1.moveToFirst();
+                                        String password = cursor1.getString(cursor1.getColumnIndex("Password"));
+                                        if(passw.equals(password)){
+                                            Toast.makeText(getBaseContext(), "Login Successful!", Toast.LENGTH_SHORT).show();
+                                            setContentView(R.layout.blank);
+                                        } else Toast.makeText(getBaseContext(), "Wrong Password!", Toast.LENGTH_SHORT).show();
+                                    }
+                                    else if(cursor2.getCount()>0){
+                                        cursor2.moveToFirst();
+                                        String passwords = cursor2.getString(cursor2.getColumnIndex("Password"));
+                                        if(passw.equals(passwords)){
+                                            Toast.makeText(getBaseContext(), "Login Successful!", Toast.LENGTH_SHORT).show();
+                                            setContentView(R.layout.blank);
+                                        } else Toast.makeText(getBaseContext(), "Wrong Password!", Toast.LENGTH_SHORT).show();
+                                    } else Toast.makeText(getBaseContext(), "Wrong Username", Toast.LENGTH_SHORT).show();
+                                } else Toast.makeText(getBaseContext(), "Password is too short!", Toast.LENGTH_SHORT).show();
+                            } else Toast.makeText(getBaseContext(), "Invalid Password!", Toast.LENGTH_SHORT).show();
+                    } else Toast.makeText(getBaseContext(), "Email or Password field must not be empty!", Toast.LENGTH_SHORT).show();
+
+                break;
+            case R.id.button2:
+                Intent intent = new Intent(MainActivity.this,register.class );
+                startActivity(intent);
+                break;
+        }
+
     }
-
-    public boolean validatePassword(String Pw) {
-        return Pw.length() >=8;
-    }
-
-    public void doLogin() {
-        Toast.makeText(getApplicationContext(), "Successfully Logged-in", Toast.LENGTH_SHORT).show();
-        Intent i = new Intent(MainActivity.this, NextScreen.class);
-
-        startActivity(i);
-
-
+    public int Login(String username,String password)
+    {
+        String[] selectionArgs = new String[]{username, password};
+        try
+        {
+            int i = 0;
+            Cursor c = null;
+            c = myDB.rawQuery("select * from Register where Username=? and Password=?", selectionArgs);
+            c.moveToFirst();
+            i = c.getCount();
+            c.close();
+            return i;
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+        return 0;
     }
 
 }
-
-
-
