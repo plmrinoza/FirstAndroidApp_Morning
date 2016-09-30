79bc54a846a25c0685fed79ab5863311d41e87ae
reynard prudente <reynard prudente>
2016-09-30 23:30:40 +0800

    final na

--- app/src/main/java/com/mlabs/bbm/firstandroidapp_morningclass/DBHelper.java
+++ app/src/main/java/com/mlabs/bbm/firstandroidapp_morningclass/DBHelper.java
@@ -1,7 +1,146 @@
 package com.mlabs.bbm.firstandroidapp_morningclass;
 
-/**
- * Created by androidstudio on 24/09/16.
- */
-public class DBHelper {
+
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.database.Cursor;
+import android.util.Log;
+import android.widget.Toast;
+
+public class DBHelper extends SQLiteOpenHelper {
+
+    //all the fields we need
+    static final String dbName ="myDB.db";
+    static final int dbVersion = 3;
+    static final String tableAccount = "tableAccount";
+    static final String id = "id";
+    static final String UN = "UN";
+    static final String EA = "email";
+    static final String FN = "firstname";
+    static final String LN = "lastname";
+    static final String PW = "PW";
+    static final String DC = "DC";
+    static final String tableCreate =  "create table tableAccount " +
+            "(id integer primary key, " +
+            UN + " text, " +
+            EA + " text, " +
+            FN + " text, " +
+            LN + " text, " +
+            PW + " text, " +
+            DC + " text);";
+    SQLiteDatabase db;
+
+    public DBHelper(Context context){
+        super(context, dbName, null, dbVersion);
+    }
+
+    //create empty table
+    @Override
+    public void onCreate(SQLiteDatabase db){
+        //every db upgrade drops previous db and needs to repopulate it manually
+        db.execSQL(tableCreate);
+        this.db = db;
+    }
+
+    //updating table settings and stuff
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion){
+        String query = "drop table if exists " + tableAccount;
+        db.execSQL(query);
+        this.onCreate(db);
+    }
+
+    //enter record to table
+    public void regAccount(Accounts a){
+        db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+
+        String query = "select * from tableAccount";
+        Cursor cursor = db.rawQuery(query,null);
+        int count = cursor.getCount();
+        values.put(id,count);
+        values.put(UN, a.getUname());
+        values.put(EA, a.getEmail());
+        values.put(FN, a.getFname());
+        values.put(LN, a.getLname());
+        values.put(PW, a.getPass());
+        values.put(DC, a.getDate());
+
+        db.insert(tableAccount, null, values);
+        db.close();
+    }
+
+    public boolean emailUnused(String email){
+        db = this.getReadableDatabase();
+        String query = "Select * from tableAccount where " + EA + " = '" + email+"'";
+        Cursor cursor = db.rawQuery(query, null);
+        if(cursor.getCount() <= 0){
+            cursor.close();
+            return true;
+        }
+        cursor.close();
+        return false;
+    }
+
+    public boolean unameUnused(String uname){
+        db = this.getReadableDatabase();
+        String query = "Select * from tableAccount where " + UN + " = '" + uname+"'";
+        Cursor cursor = db.rawQuery(query, null);
+        if(cursor.getCount() <= 0){
+            cursor.close();
+            return true;
+        }
+        cursor.close();
+        return false;
+    }
+
+    public String matchPassEA (String email){
+        db = this.getWritableDatabase();
+        String query = "select "+ EA +", "+ PW +" from " + tableAccount;
+        Cursor cursor = db.rawQuery(query,null);
+        String e, p;
+        p = null;
+        if(cursor.moveToFirst()){
+            do{
+                e = cursor.getString(cursor.getColumnIndex("email"));
+
+                if(e.equals(email)){
+                    p = cursor.getString(cursor.getColumnIndex("PW"));
+                    break;
+                }
+
+            }while(cursor.moveToNext());
+        }
+        return p;
+    }
+
+    public String matchPassUN (String uname){
+        db = this.getWritableDatabase();
+        String query = "select " + UN + " ," + PW + " from " + tableAccount;
+        Cursor cursor = db.rawQuery(query,null);
+        String e, p;
+        p = null;
+        if(cursor.moveToFirst()){
+            do{
+                e = cursor.getString(cursor.getColumnIndex("UN"));
+
+                if(e.equals(uname)){
+                    p = cursor.getString(cursor.getColumnIndex("PW"));
+                    break;
+                }
+            }while(cursor.moveToNext());
+        }
+        return p;
+    }
+
+    public Cursor getAllData() {
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor res = db.rawQuery("select * from "+ tableAccount,null);
+        return res;
+    }
 }
+
--- app/src/main/java/com/mlabs/bbm/firstandroidapp_morningclass/sign_up.java
+++ app/src/main/java/com/mlabs/bbm/firstandroidapp_morningclass/sign_up.java
@@ -1,13 +1,161 @@
 package com.mlabs.bbm.firstandroidapp_morningclass;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class sign_up extends AppCompatActivity {
+    EditText email, uname, fname, lname, pass, cPass;
+    Button reg, viewAll;
+    DBHelper helper = new DBHelper(this);
+    Pattern emailP;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        email = (EditText) findViewById(R.id.ea);
+        uname = (EditText) findViewById(R.id.un);
+        fname = (EditText) findViewById(R.id.fn);
+        lname = (EditText) findViewById(R.id.ln);
+        pass = (EditText) findViewById(R.id.pass);
+        cPass = (EditText) findViewById(R.id.cp);
+        reg = (Button) findViewById(R.id.create);
+        viewAll = (Button) findViewById(R.id.view);
+        emailP = Pattern.compile("^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$");
+        findViewById(R.id.create).setOnClickListener(createClickListener);
+        viewAll();
+    }
+    View.OnClickListener createClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v){
+            String emailStr = email.getText().toString().toLowerCase();
+            String unameStr = uname.getText().toString().toLowerCase();
+            String fnameStr = fname.getText().toString().toLowerCase();
+            String lnameStr = lname.getText().toString().toLowerCase();
+            String pwStr = pass.getText().toString();
+            String cPWStr = cPass.getText().toString();
+            String eMsg;
+
+            eMsg = validate(emailStr,unameStr,pwStr,cPWStr,fnameStr,lnameStr);
+            if(eMsg == null){
+                Accounts a = new Accounts();
+                a.setEmail(emailStr);
+                a.setUname(unameStr);
+                a.setFname(fnameStr);
+                a.setLname(lnameStr);
+                a.setPass(pwStr);
+                a.setDate();
+
+                helper.regAccount(a);
+                //pop up dialog box
+                AlertDialog alertDialog = new AlertDialog.Builder(sign_up.this).create();
+                alertDialog.setMessage("Account Successfully Created");
+                alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                                //back to loginScreen
+                                //MainActivity is already gone, start new one
+                                finish();
+                                Intent intent = new Intent(sign_up.this,MainActivity.class );
+                                startActivity(intent);
+                            }
+                        });
+                alertDialog.show();
+            }else
+                Toast.makeText(getApplicationContext(), eMsg, Toast.LENGTH_SHORT).show();
+        }
+    };
+    String validate(String email, String uname, String pw, String cpw, String fname, String lname) {
+        Matcher m;
+        m = emailP.matcher(email);
+        if (m.matches() && pw.length()>=8){
+            Log.d("TEST","valid input");
+            if (pw.equals(cpw)){
+                Log.d("TEST", "password match");
+                if(helper.emailUnused(email))
+                    if(helper.unameUnused(uname))
+                        if(isAlpha(fname)&&isAlpha(lname))
+                            return null;
+                        else
+                            return "Invalid name";
+                    else
+                        return "Username already used.";
+                else
+                    return "Email already used";
+            }
+            else
+                return "Password does not match";
+        }
+        else {
+            return "Invalid Email/Password";
+        }
+    }
+
+    public boolean isAlpha(String name){
+        return name.matches("^[\\p{L} .'-]+$"); //letter and spaces only
+    }
+
+//back button goes back to loginScreen
+@Override
+public boolean onKeyDown(int keyCode, KeyEvent event) {
+    if (keyCode == KeyEvent.KEYCODE_BACK) {
+        finish();
+        Intent intent = new Intent(sign_up.this,MainActivity.class );
+        startActivity(intent);
+    }
+    return super.onKeyDown(keyCode, event);
+}
+
+    public void viewAll() {
+        viewAll.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Cursor res = helper.getAllData();
+                        if(res.getCount() == 0) {
+                            // show message
+                            showMessage("Error","Nothing found");
+                            return;
+                        }
+
+                        StringBuffer buffer = new StringBuffer();
+                        while (res.moveToNext()) {
+                            buffer.append("id :"+ res.getString(0)+"\n");
+                            buffer.append("UN :"+ res.getString(1)+"\n");
+                            buffer.append("email :"+ res.getString(2)+"\n");
+                            buffer.append("firstname :"+ res.getString(3)+"\n");
+                            buffer.append("lastname :"+ res.getString(4)+"\n");
+                            buffer.append("PW :"+ res.getString(5)+"\n");
+                            buffer.append("DC :"+ res.getString(6)+"\n\n");
+                        }
+
+                        // Show all data
+                        showMessage("Data",buffer.toString());
+                    }
+                }
+        );
+    }
+
+    public void showMessage(String title,String Message){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setTitle(title);
+        builder.setMessage(Message);
+        builder.show();
     }
 }
