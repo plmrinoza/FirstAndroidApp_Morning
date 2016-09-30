cbaba944d14fa9c71b3ca1677493ce1677bea8ea
eppaguinaldo <eppaguinaldo@gmail.com>
2016-09-30 16:10:31 +0800

    Added Register validation, refuses duplicate entries on Username and Email

--- app/src/main/java/com/mlabs/bbm/firstandroidapp_morningclass/Register.java
+++ app/src/main/java/com/mlabs/bbm/firstandroidapp_morningclass/Register.java
@@ -1,7 +1,9 @@
 package com.mlabs.bbm.firstandroidapp_morningclass;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -18,16 +20,18 @@ import android.text.Spanned;
  * Created by Earl on 22/09/2016.
  */
 public class Register extends Activity {
-    DatabaseHelper myDb;
+    DatabaseHelper myDb, db;
     EditText editEmail, editPassword, editCPassword, editFname, editLname, editUname;
     Button btnAddData;
-    String email, pass, cpass;
+    String email, pass, cpass, uname;
+    Context CTX = this;
 
     @Override
     protected void onCreate (Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register_layout);
         myDb = new DatabaseHelper(this);
+
 
         editFname = (EditText)findViewById(R.id.FnameReg);
         editLname = (EditText)findViewById(R.id.LnameReg);
@@ -36,6 +40,8 @@ public class Register extends Activity {
         editPassword = (EditText)findViewById(R.id.PassReg);
         editCPassword = (EditText)findViewById(R.id.ConfirmPass);
         btnAddData = (Button)findViewById(R.id.regibtn);
+
+        editFname.setFocusable(true);
 
         //To restrict Fname and Lname edittext from accepting numbers as well as special characters
         editFname.setFilters(new InputFilter[]{
@@ -72,7 +78,9 @@ public class Register extends Activity {
     public void AddData() {
         btnAddData.setOnClickListener(new View.OnClickListener() {
                     @Override
-                    public void onClick(View v) {
+                    public void onClick(View v)
+                    {
+                        uname = editUname.getText().toString();
                         email = editEmail.getText().toString();
                         pass = editPassword.getText().toString();
                         cpass = editCPassword.getText().toString();
@@ -84,25 +92,51 @@ public class Register extends Activity {
                         }
 
                         //Checks if passwords match after confirming the email and passwords meet the required format
-                        if (isValidEmail(email) && isValidPassword(pass))
-                        {
-                            if(!(pass.equals(cpass))){
-                                Toast.makeText(Register.this, "Passwords do not match", Toast.LENGTH_LONG).show();
-                                editCPassword.requestFocus();
-                            }
-                            else
+                        db = new DatabaseHelper(CTX);
+                        Cursor cr = db.getInfo(db);
+                        cr.moveToFirst();
+                        boolean unamestat = false, emailstat = false;
+                        do {
+                            if ((uname.equals(cr.getString(3)))) //checks database if entered information is present
                             {
-                                Date curDate = new Date();
-                                SimpleDateFormat format = new SimpleDateFormat("MM-dd-yyyy");
-                                String DateToStr = format.format(curDate);
-                                boolean isInserted = myDb.insetData(editFname.getText().toString(), editLname.getText().toString(), editUname.getText().toString(),editEmail.getText().toString(), editPassword.getText().toString(), DateToStr);
-                                if(isInserted == true)
-                                {
-                                    Toast.makeText(Register.this, "Registration Successful", Toast.LENGTH_LONG).show();
-                                    Intent intent = new Intent(Register.this, Login.class);
-                                    startActivity(intent);
-                                    finish();
+                                unamestat = true;
+                            }
+                            if ((email.equals(cr.getString(4)))){
+                                emailstat = true;
+                            }
+                        } while (cr.moveToNext());
+
+                        if(unamestat == false && emailstat == false)
+                        {
+                            if (isValidEmail(email) && isValidPassword(pass))
+                            {
+                                if(!(pass.equals(cpass))){
+                                    Toast.makeText(Register.this, "Passwords do not match", Toast.LENGTH_LONG).show();
+                                    editCPassword.requestFocus();
                                 }
+                                else
+                                {
+                                    Date curDate = new Date();
+                                    SimpleDateFormat format = new SimpleDateFormat("MM-dd-yyyy");
+                                    String DateToStr = format.format(curDate);
+                                    boolean isInserted = myDb.insetData(editFname.getText().toString(), editLname.getText().toString(), editUname.getText().toString(),editEmail.getText().toString(), editPassword.getText().toString(), DateToStr);
+                                    if(isInserted == true)
+                                    {
+                                        Toast.makeText(Register.this, "Registration Successful", Toast.LENGTH_LONG).show();
+                                        Intent intent = new Intent(Register.this, Login.class);
+                                        startActivity(intent);
+                                        finish();
+                                    }
+                                }
+                            }
+                        }
+                        else
+                        {
+                            if(unamestat == true){
+                                editUname.setError("Username Already in Use");
+                            }
+                            if(emailstat == true){
+                                editEmail.setError("Email Already in Use");
                             }
                         }
 
